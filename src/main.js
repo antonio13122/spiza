0975e1d7dbedf9249138b049b509651bcbadbc9e
antonio13122 <tonisajbic501@gmail.com>
2024-03-12 16:18:51 +0100

    installed stripe

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store1';
-
+import Vuex from 'vuex';
 // Import your Vuex store instance
 
 const app = createApp(App);
@@ -12,6 +12,6 @@ app.use(store);
 
 app.mount('#app');
 
-
+app.use(Vuex);
 
 
